5cd58648d8cc1e6819422dd2c757e6dc815c9b44
ChiaoChiehChen <qa6248@gmail.com>
2022-02-13 03:19:28 +0800

    products

--- routes/products.js
+++ routes/products.js
@@ -4,11 +4,17 @@ import auth from '../middleware/auth.js'
 import admin from '../middleware/admin.js'
 import upload from '../middleware/upload.js'
 import {
-  create
+  create,
+  getProducts,
+  getAllProducts,
+  getProductById
 } from '../controllers/products.js'
 
 const router = express.Router()
 
 router.post('/', auth, admin, content('multipart/form-data'), upload, create)
+router.get('/', getProducts)
+router.get('/', auth, admin, getAllProducts)
+router.get('/:id', getProductById)
 
 export default router
